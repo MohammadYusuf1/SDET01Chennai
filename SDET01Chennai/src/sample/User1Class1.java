7a5c138db39a23560f39753bcfd2cfe07677e5c1
Mohammad Yusuf <Mohammad Yusuf@TYSS-Mohammad>
2021-12-20 17:54:50 +0530

    User1 updated the class

--- SDET01Chennai/src/sample/User1Class1.java
+++ SDET01Chennai/src/sample/User1Class1.java
@@ -5,6 +5,7 @@ public class User1Class1 {
 	public static void main(String[] args) {
 		
 		System.out.println("User 1 has created a class and pushed to GitHub");
+		System.out.println("User 1 modified the class");
 
 	}
 }
